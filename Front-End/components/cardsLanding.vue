d6acc10f48744f77af8595fc62050b5e07cf3d3e
Tonnellier <gael.tonnellier@gmail.com>
2021-07-22 15:21:54 +0200

    composant accueil

---
+++ Front-End/components/cardsLanding.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-col class=" ma-5 d-flex justify-center background rounded-lg elevation-1">
+    <v-col class="d-flex flex-column justify-center">
+      <p class="text-center">
+        {{ description }}
+      </p>
+      <v-icon class="mt-auto" color="secondary" size="160px"
+        >{{ icon }}</v-icon
+      >
+      <v-btn :to="link" nuxt class="primary"> {{ title }} </v-btn>
+    </v-col>
+  </v-col>
+</template>
+
+<script>
+export default {
+    props:['description', 'title', 'icon','link']
+};
+</script>
+
+<style></style>
--- Front-End/pages/landing.vue
+++ Front-End/pages/landing.vue
@@ -1,61 +1,53 @@
 <template>
-      <v-card class=" pa-4">
-        <h1 class="ma-5 text-center">
-          Bienvenue sur l'application Groupomania !
-        </h1>
-        <p class="text-h5 text-center">
-          C'est ici que vous retrouverez vos collègues de travail ainsi que tout
-          les sujets du moments sous forme de GIF ou d'image ! Allez jetez un
-          oeil aux différentes catégories du sites pour en savoir plus !
-        </p>
-        <v-row class="pa-5">
-          <v-col
-            class=" ma-5 d-flex justify-center background rounded-lg elevation-1"
-          >
-            <v-col class="d-flex flex-column justify-center">
-              <p class="text-center" >
-                Pour modifier votre pseudo, email, description, avatar ou encore
-                pour supprimer votre compte c'est par ici !
-              </p>
-              <v-icon class="mt-auto" color="secondary"  size="160px">mdi-account-box</v-icon>
-              <v-btn to="/account" nuxt class="primary">Mon compte</v-btn>
-            </v-col>
-          </v-col>
-          <v-col
-            class=" ma-5 d-flex justify-center background rounded-lg elevation-1"
-          >
-            <v-col class="d-flex flex-column justify-center ">
-              <p class="text-center" >
-                Envie de savoir comment fonctionne l'application, ou d'en savoir un peu plus sur certaine fonctionnalité du site ? Clique sur le bouton ci-dessous !
-              </p>
-              <v-icon class="mt-auto" color="secondary"  size="160px" >mdi-help-circle</v-icon>
-              <v-btn to="/faq" nuxt class="primary"> aide </v-btn>
-            </v-col>
-          </v-col>
-          <v-col
-            v-if="connected=true" class="ma-5 d-flex justify-center background rounded-lg elevation-1"
-          >
-            <v-col class="d-flex flex-column justify-center">
-              <p class="text-center">
-                Le mur c'est "The place to be !" ** pour passer un bon moment ! <br/>
-                La page où sont affichés  les messages des collègues ! <br/>
-                <span class="font-italic font-weight-light">The place to be = l'endroit idéal</span>
-              </p>
-              <v-icon class="mt-auto" color="secondary"  size="160px" >mdi-wall</v-icon>
-              <v-btn to="/wall" nuxt class="primary">le mur</v-btn>
-            </v-col>
-          </v-col>
-        </v-row>
-      </v-card>
+  <v-card class=" pa-4">
+    <h1 class="ma-5 text-center">
+      Bienvenue sur l'application Groupomania !
+    </h1>
+    <p class="text-h5 text-center">
+      C'est ici que vous retrouverez vos collègues de travail ainsi que tout les
+      sujets du moments sous forme de GIF ou d'image ! Allez jetez un oeil aux
+      différentes catégories du sites pour en savoir plus !
+    </p>
+    <v-row class="pa-5">
+      <cardsLanding v-for="card in cards" :key="card.id"
+        v-bind:title="card.title" v-bind:description="card.description" v-bind:icon="card.icon" v-bind:link="card.link"
+      />
+    </v-row>
+  </v-card>
 </template>
 
 <script>
+import cardsLanding from "~/components/cardsLanding.vue";
 export default {
   layout: "home",
-  data: ()=>({
-      connected: false,
+  components: { cardsLanding },
+  data: () => ({
+    connected: false,
+    cards: [
+      {
+        description:
+          "Pour modifier votre pseudo, email, description, avatar ou encore pour supprimer votre compte c'est par ici !",
+        icon: "mdi-account-box",
+        title: "Mon compte",
+        link:"account"
+      },
+      {
+        description:
+          "Envie de savoir comment fonctionne l'application, ou d'en savoir un peu plus sur certaine fonctionnalité du site ? Clique sur le bouton ci-dessous !!",
+        icon: "mdi-help-circle",
+        title: "aide",
+        link:"faq"
+      },
+      {
+        description:
+          " Le mur c'est l'endroit où aller  pour passer un bon moment ! La page où sont affichés  les messages des collègues !",
+        icon: "mdi-wall",
+        title: "le mur",
+        link: "wall"
+      }
+    ]
   }),
-  mounted: function(){
+  mounted: function() {
     this.connected = this.$store.state.user.userId === -1;
   }
 };
